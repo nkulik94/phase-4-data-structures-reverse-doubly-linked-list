57831bf152d230700ab0ba9a4feec83b425961a1
Naftali Kulik <nkulik1994@gmail.com>
2022-08-03 15:27:45 -0400

    commit tree ee81d747c31390b66739fda9494034549f663cf4

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,29 @@ class LinkedList
     @tail = nil
   end
 
+  def reverse_node node
+    if node.prev_node
+      if !node.next_node
+        node.next_node = node.prev_node
+        node.prev_node = nil
+        self.head = node
+      else
+        old_next = node.next_node
+        node.next_node = node.prev_node
+        node.prev_node = old_next
+      end
+      reverse_node(node.next_node)
+    else
+      node.prev_node = node.next_node
+      node.next_node = nil
+      self.tail = node
+    end
+  end
+
   def reverse!
-    # your code here
+    if !self.head
+      return
+    end
+    reverse_node(self.tail)
   end
 end
